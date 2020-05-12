b0de0d2c9e34d741a5c78efcf6b07900fec46876
Erik Seglem <erik.seglem@gmail.com>
2020-05-11 20:22:38 -0400

    Icrement version.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="pywattbox",
-    version="0.2.0",
+    version="0.3.0",
     author="Erik Seglem",
     author_email="erik.seglem@gmail.com",
     description="A python wrapper for the WattBox API.",
     url="https://github.com/eseglem/pywattbox",
-    license='MIT',
+    license="MIT",
     packages=setuptools.find_packages(),
-    install_requires=['requests', 'beautifulsoup4', 'lxml'],
+    install_requires=["requests", "beautifulsoup4", "lxml"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
